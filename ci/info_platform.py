2f26416ad8d0644329ead5b41daca8ba7c2ae60a
woutdenolf <woutdenolf@users.sf.net>
2018-03-11 18:02:38 +0100

    CI bugs

--- ci/info_platform.py
+++ ci/info_platform.py
@@ -3,9 +3,11 @@
 # Original script from https://github.com/silx-kit/pyFAI
 
 import sys, numpy
+
 print("Python %s bits" % (tuple.__itemsize__ * 8))
 print("       maxsize: %s\t maxunicode: %s" % (sys.maxsize, sys.maxunicode))
 print(sys.version)
+
 try:
     from distutils.sysconfig import get_config_vars
 except:
@@ -15,11 +17,13 @@ try:
     print("Config :" + " ".join(config))
 except:
     print("Config : None")
+
 print("")
 print("Numpy %s" % numpy.version.version)
 print("      include %s" % numpy.get_include())
 print("      options %s" % numpy.get_printoptions())
 print("")
+
 try:
     import pyopencl
 except Exception as error:
